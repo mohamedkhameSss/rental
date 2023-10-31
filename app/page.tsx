94839c74c9e89b6d3de0fe4c33e73e9cd8a392de
mohamedkhameSss <113071196+mohamedkhameSss@users.noreply.github.com>
2023-10-31 03:01:15 +0200

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import getListing, { IListingsParams } from "./actions/getListing";
 
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
+export const dynamic = 'force-dynamic'
 
 interface HomeProps {
   searchParams: IListingsParams;
